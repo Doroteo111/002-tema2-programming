6576436d2b2b6cc46ef7e5309434865f198657d5
Doroteo111 <ladoritaesta@gmail.com>
2022-10-10 10:41:23 +0200

    Scripts_vehiculos

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    /*
+    4 variables with the player information
+     */
+    public int playerAge = 33; //This variable saves the player's age
+    public float playerSpeed = 3.125f;
+    public string playerName = "Isa De";
+    public bool gameOver = false;
+}
