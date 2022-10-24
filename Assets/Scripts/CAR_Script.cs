1df898b72d1afe7c8f66d842ce9fa42f8f1e4e8c
Doroteo111 <ladoritaesta@gmail.com>
2022-10-24 10:47:50 +0200

    operaciones de decision

--- Assets/Scripts/CAR_Script.cs
+++ Assets/Scripts/CAR_Script.cs
@@ -7,8 +7,42 @@ public class CAR_Script : MonoBehaviour
     [SerializeField] private int Wheels = 4; //amount of wheels the vehicle has 
     [SerializeField] private string Name = "motobike";
 
+
+    [SerializeField] private bool isOn;
+
+    public string Sound;
+
     private void Start()
     {
         Debug.Log(string.Format("The {0} has {1} Wheels", Name, Wheels));
+
+       /* if (isOn == true) //encendido o no
+            Debug.Log($"El {Name} esta encendido");
+
+        else
+            Debug.Log($"El {Name} esta apagado"); */
+
+        if (isOn == false)
+        {
+            Debug.Log($"El {Name} esta apagado");
+        }
+
+
+        else if (Sound != "")
+        {
+            Debug.Log($"El {Name} esta encendido y hace {Sound}");
+        }
+           
+        else
+        {
+            Debug.Log($"El {Name} esta en marcha");
+        }
+
+
+
+
+
+
+
     }
 }                                                                                    
--- Assets/Scripts/Operation.cs
+++ Assets/Scripts/Operation.cs
@@ -6,13 +6,27 @@ public class Operation : MonoBehaviour
 {
     public int x = 7;
     public int y = 4;
+
+    public bool isRaining;
+
+    public int number;
     private void Start()
     {
-        Debug.Log(x+y);
-        Debug.Log(x-y);
-        Debug.Log(x*y);
-        Debug.Log(x/y);
-        Debug.Log(x%y);
+        Debug.Log(x + y);
+        Debug.Log(x - y);
+        Debug.Log(x * y);
+        Debug.Log(x / y);
+        Debug.Log(x % y);
+
+        if (isRaining == true) //decision operation
+
+            Debug.Log("Recuerda pillar el paraguas");
+        else Debug.Log("No esta lloviendo, puedes dejar el paraguas");
+
+        if (number > 0)
+            Debug.Log($"El número {number} es positivo");
+
+
 
 
     }
